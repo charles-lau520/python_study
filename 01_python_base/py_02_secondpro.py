af44484b3e8df25265ba56d1a2943c2395b9a95e
charles-lau520 <liuxiaochuan@aliyun.com>
2020-06-10 16:56:21 +0800

    20200610

--- 01_python_base/py_02_secondpro.py
+++ 01_python_base/py_02_secondpro.py
@@ -1,8 +1,21 @@
 # 这是注释版本
 print("hello")
-
 # 这是第二个注释
 print("hello python") #输出欢迎信息
+"""
+多行注释
+1.
+2.
+3.
+"""
+print('-'*10)
 
-
+qq_number = "1234567"
+qq_password = "123"
+print(qq_number)
+print(qq_password)
+price = 8.5
+weight = 7.5
+money = price*weight
+print(money)
 
