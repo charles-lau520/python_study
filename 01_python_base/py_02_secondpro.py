cb75c9257a37b63ab95e94447c1c4f2b3069918b
charles-lau520 <liuxiaochuan@aliyun.com>
2020-06-17 11:39:01 +0800

    20200617

--- 01_python_base/py_02_secondpro.py
+++ 01_python_base/py_02_secondpro.py
@@ -1,7 +1,7 @@
 # 这是注释版本
 print("hello")
 # 这是第二个注释
-print("hello python") #输出欢迎信息
+print("hello python")  # 输出欢迎信息
 """
 多行注释
 1.
@@ -9,32 +9,38 @@ print("hello python") #输出欢迎信息
 3.
 """
 # -----------------------------------
-print("-"*10+"我是分割线"+"-"*10)
+print("-" * 10 + "我是分割线" + "-" * 10)
 qq_number = "1234567"
 qq_password = "123"
 print(qq_number)
 print(qq_password)
 price = 8.5
 weight = 7.5
-money = price*weight
-print("应支付:"+str(money))
-money = money-5
-print("优惠价格:"+str(money))
-print(str(price+weight))
+money = price * weight
+print("应支付:" + str(money))
+money = money - 5
+print("优惠价格:" + str(money))
+print(str(price + weight))
 # -----------------------------------
-print("-"*10+"我是分割线"+"-"*10)
+print("-" * 10 + "我是分割线" + "-" * 10)
 name = "小明"
 age = 18
 sex = "男生"
 high = 1.75
 weight = 75.0
-print(name+"今年"+str(age)+"岁，是一个"+sex+"，身高"+str(high)+"米，体重"+str(weight)+"公斤。")
+print(name + "今年" + str(age) + "岁，是一个" + sex + "，身高" + str(high) + "米，体重" + str(weight) + "公斤。")
 first_name = "AB"
 last_name = "CD"
-print(first_name+last_name)
-print((first_name+last_name)*5)
+print(first_name + last_name)
+print((first_name + last_name) * 5)
 # -----------------------------------
-print("-"*10+"我是分割线"+"-"*10)
+print("-" * 10 + "我是分割线" + "-" * 10)
 number = input("请输入：")
 print(number)
-
+# -----------------------------------
+student_id = 1
+print("我的学号是 %06d" %student_id)
+name = "hello"
+print("我的名字叫 %s，请多多关照,%s %s" %(name,first_name,last_name))
+percent = 0.08
+print("数据比例：%.2f%%" % (percent * 100))
