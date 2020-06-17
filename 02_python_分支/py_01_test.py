7011213eb50e42737c33bc45e369ce1d13074dbe
charles-lau520 <liuxiaochuan@aliyun.com>
2020-06-17 16:21:29 +0800

    20200617

---
+++ 02_python_分支/py_01_test.py
@@ -0,0 +1,20 @@
+age = 17
+# age = int(input("请输入年龄:"))
+if age >= 18:
+    print("年龄大于18岁，您已成年")
+else:
+    print("年龄小于18岁，还未成年")
+
+age = 1200
+
+if age >= 0 or age <= 120:
+    print("年龄符合")
+else:
+    print("年龄不符合")
+
+python_score = 50
+c_score = 49
+if python_score>=60 or c_score>=60:
+    print("合格")
+else:
+    print("不合格")
